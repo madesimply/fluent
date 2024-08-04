d7d78040fd66e86afb85ac8563ee0533fb07462e
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2024-08-04 17:32:44 -0400

    rm example config

--- tsup.config.js
+++ tsup.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'tsup'
 
-const lib = {
-  entry: [ 'src/index.ts' ],
-  outDir: 'dist',
-  dts: true,
-  format: ['cjs', 'esm'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-}
-
-const examples = {
-  entry: [ 'examples/index.ts' ],
-  outDir: 'examples/dist',
-  clean: true,
-}
-
 export default defineConfig((options) => {
   const isExamples = (options?.entry || []).includes('examples');
-  return isExamples ? examples : lib
+  return {
+    entry: [ 'src/index.ts' ],
+    outDir: 'dist',
+    dts: true,
+    format: ['cjs', 'esm'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+  }
 })
