74f55774b40ab6e951035b1b5e4485dfcc2fedfd
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2024-08-31 12:34:04 -0400

    update build and ts setup

--- tsup.config.js
+++ tsup.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig(() => {
-  return {
-    entry: ["src/index.ts"],
-    outDir: "dist",
-    dts: true,
-    format: ["cjs", "esm"],
-    minify: true,
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    tsconfig: "./tsconfig.json", // Add this line
-  };
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  minify: true,
+  target: 'es2020',
+  tsconfig: "./tsconfig.json",
 });
