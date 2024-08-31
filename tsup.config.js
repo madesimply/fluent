735562b066a616c0478e1e3d71dd0ba599e81340
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2024-08-31 12:21:51 -0400

    cleanup deps / build

--- tsup.config.js
+++ tsup.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig(() => {
   return {
-    entry: [ 'src/index.ts' ],
-    outDir: 'dist',
+    entry: ["src/index.ts"],
+    outDir: "dist",
     dts: true,
-    format: ['cjs', 'esm'],
-    // minify: true,
+    format: ["cjs", "esm"],
+    minify: true,
     splitting: false,
     sourcemap: true,
     clean: true,
-  }
-})
+  };
+});
